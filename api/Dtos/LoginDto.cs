635eac47d065ff139d6ba7d481a311cafc46bd5e
Nektarios Raftis <nektarios.raftis@epicstudios.org>
2025-05-18 19:48:45 +0300

    test

--- api/Dtos/LoginDto.cs
+++ api/Dtos/LoginDto.cs
@@ -11,6 +11,8 @@ namespace api.Dtos
         [Required] 
         public string UserName { get;  set;}
         public string Password { get;  set;}
+        public string asd { get;  set;}
+        public string aaasd { get;  set;}
 
     }
 }
