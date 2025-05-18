3a5ebe4b12f2b23a2d948a0ba1b7dfce45c00072
Κωνσταντίνα Νίκα <kwnstantinanika@gmail.com>
2025-05-18 19:51:34 +0300

    test

--- api/Dtos/LoginDto.cs
+++ api/Dtos/LoginDto.cs
@@ -11,6 +11,8 @@ namespace api.Dtos
         [Required] 
         public string UserName { get;  set;}
         public string Password { get;  set;}
+        public string aaa { get;  set;}
+        public string 22 { get;  set;}
 
     }
 }
